50355c9d478e17e6e1f5fbb56096b127a767c99d
wangweiqing <weiqingking@gmail.com>
2014-07-28 23:45:39 +0800

    Before&&After Advice test!

--- src/main/resources/java/com/wwq/calculator/CalculatorLoggingAspect.java
+++ src/main/resources/java/com/wwq/calculator/CalculatorLoggingAspect.java
@@ -1,7 +1,11 @@
 package com.wwq.calculator;
 
+import java.util.Arrays;
+
 import org.apache.log4j.Logger;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -10,12 +14,25 @@ public class CalculatorLoggingAspect {
 	/**
 	 * Logger for this class
 	 */
-	private static final Logger logger = Logger.getLogger(CalculatorLoggingAspect.class);
+	private static final Logger logger = Logger
+			.getLogger(CalculatorLoggingAspect.class);
 
 	
 	@Before("execution(* ArithmeticCalculator.add(..))")
 	public void logBefore(){
 		logger.info("The method add() begins");
+	}
+	
+	@Before("execution(* *.*(..))")
+	public void logBefore(JoinPoint joinPoint){
+		logger.info("The method " + joinPoint.getSignature().getName()
+				+ "() begins with " + Arrays.toString(joinPoint.getArgs()));
+	}
+	
+	@After("execution(* *.*(..))")
+	public void logAfter(JoinPoint joinPoint){
+		logger.info("The method " + joinPoint.getSignature().getName()
+				+ "() ends");
 	}
 	/**
 	 * @param args
