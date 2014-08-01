3cc5cddafff2cb5f6f8b6cb274c2d9cbaa7193e6
wangweiqing <weiqingking@gmail.com>
2014-08-01 15:48:49 +0800

    TransactionTemplate with TransactionCallbackWithoutResult or TransactionCallback<T>

--- src/main/resources/java/com/wwq/bookshop/TransactionalJdbcBookShop.java
+++ src/main/resources/java/com/wwq/bookshop/TransactionalJdbcBookShop.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class TransactionalJdbcBookShop extends JdbcDaoSupport implements
 		BookShop {
@@ -17,9 +19,9 @@ public class TransactionalJdbcBookShop extends JdbcDaoSupport implements
 	}
 
 	@Override
-	public void purchase(String isbn, String username) {
+	public void purchase(final String isbn, final String username) {
 		// TODO Auto-generated method stub
-		TransactionDefinition def = new DefaultTransactionDefinition();
+/*		TransactionDefinition def = new DefaultTransactionDefinition();
 		TransactionStatus status = transactionManager.getTransaction(def);
 		try {
 			int price = getJdbcTemplate().queryForInt(
@@ -37,7 +39,23 @@ public class TransactionalJdbcBookShop extends JdbcDaoSupport implements
 		} catch (DataAccessException e) {
 			transactionManager.rollback(status);
 			throw e;
-		}
+		}*/
+		TransactionTemplate transactionTemplate = new TransactionTemplate(
+				transactionManager);
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				int price = getJdbcTemplate().queryForInt(
+						"SELECT PRICE FROM BOOK WHERE ISBN = ?",
+						new Object[] { isbn });
+				getJdbcTemplate().update(
+						"UPDATE BOOK_STOCK SET STOCK = STOCK - 1 "
+								+ "WHERE ISBN = ?", new Object[] { isbn });
+				getJdbcTemplate().update(
+						"UPDATE ACCOUNT SET BALANCE = BALANCE - ? "
+								+ "WHERE USERNAME = ?",
+						new Object[] { price, username });
+			}
+		});
 
 	}
 
