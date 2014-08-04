e4f7bb1287e9e0a1e882a3fab36f0b314905191e
wangweiqing <weiqingking@gmail.com>
2014-08-04 17:25:09 +0800

    web flow there is something wrong with the Controller

---
+++ src/main/resources/java/com/wwq/court/web/ReservationQueryController.java
@@ -0,0 +1,39 @@
+package com.wwq.court.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.wwq.court.service.ReservationService;
+import com.wwq.court.domain.Reservation;
+
+@Controller
+@RequestMapping("/reservationQuery")
+public class ReservationQueryController {
+
+	private ReservationService reservationService;
+
+	public void ReservationQueryController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public void setupForm() {
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String sumbitForm(@RequestParam("courtName") String courtName,
+			Model model) {
+		List<Reservation> reservations = java.util.Collections.emptyList();
+		if (courtName != null) {
+			reservations = reservationService.query(courtName);
+		}
+		model.addAttribute("reservations", reservations);
+		return "reservationQuery";
+	}
+}
